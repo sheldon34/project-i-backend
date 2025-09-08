a8832545ebf3c21a0707463b8cb09420a9ff004d
sheldon34 <mubokasheldonmuboka@gmail.com>
2025-09-09 00:07:05 +0300

    cors-fixed

--- src/main/java/com/example/securityskilltesting/security/CorsConfig.java
+++ src/main/java/com/example/securityskilltesting/security/CorsConfig.java
@@ -15,7 +15,8 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("https://project-i-frontend-6m7l.vercel.app/");
+        config.addAllowedOriginPattern("https://project-i-frontend-6m7l.vercel.app/");
+        config.addAllowedOriginPattern("http://localhost:5173");
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With", "Accept"));
 
